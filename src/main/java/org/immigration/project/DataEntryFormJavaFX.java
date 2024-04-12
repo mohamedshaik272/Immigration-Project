f6914f6a59e4d98f39db663931b2e070f5799710
mohamedshaik272 <mohamedshaik272@gmail.com>
2024-04-12 19:55:16 -0400

    Changes

---
+++ src/main/java/org/immigration/project/DataEntryFormJavaFX.java
@@ -0,0 +1,16 @@
+package org.immigration.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DataEntryFormJavaFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
