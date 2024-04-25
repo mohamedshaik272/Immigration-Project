091bf298bf42ad865eb5aa011443d29165fb8112
San68bot <san68bot@gmail.com>
2024-04-25 12:32:10 -0400

    Added controller class for reviewer

--- src/main/java/org/immigration/project/OneUI.java
+++ src/main/java/org/immigration/project/OneUI.java
@@ -11,17 +11,14 @@ import java.io.IOException;
 import static org.immigration.project.Globals.*;
 
 public class OneUI extends Application {
-    private StageController stageController;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        stageController = new StageController(primaryStage)
+        new StageController(primaryStage)
                 .add("oneui", "oneUI.fxml")
                 .add("login", "login.fxml")
                 .add(UserType.DATAENTRY.fx_name, UserType.DATAENTRY.fx_file)
                 .add(UserType.REVIEWER.fx_name, UserType.REVIEWER.fx_file)
                 .add(UserType.APPROVAL.fx_name, UserType.APPROVAL.fx_file);
-
         StageController.activate("oneui");
     }
 
